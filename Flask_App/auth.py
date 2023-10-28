3415aa53c163101c79a561749ff9c23f38c3e4ff
Sandra Udeike <113645774+Sandymaramma@users.noreply.github.com>
2023-10-28 06:23:54 +0100

    update

--- Flask_App/auth.py
+++ Flask_App/auth.py
@@ -1,6 +1,4 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
-from .models import User
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
@@ -13,29 +11,8 @@ def login():
 def logout():
     return "<p>Logout</p>"
 
-
-
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
-        email = request.form.get('email')
-        firstName = request.form.get('firstName')
-        password1 = request.form.get('password1')
-        password2 = request.form.get('password2')
-
-        if len(email) < 4:
-            flash('Email must be greater than 3 characters.', category='error')
-        elif len(firstName) < 2:
-            flash('First name must be greater than 1 character.', category='error')
-        elif password1 != password2:
-            flash('Password don\'t match', category='error')
-        elif len(password1) < 7:
-            flash('Password must be at least 7 characters.', category='error')
-        else:
-            new_user = User(email=email, fitstName=firstName, password=generate_password_hash(password1, method='sha256)'))
-            db.session.add(new_user)
-            db.session.commit()
-            flash('Account created!', category='success')
-            return redirect(url_for('views.home'))
 
     return render_template("sign_up.html")
--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-from Flask_App.__init__ import create_app
-
-import sys
-import os
-
-from Simple_Flask_App import create_app
+from Flask_App import create_app
 
 app = create_app()
 
